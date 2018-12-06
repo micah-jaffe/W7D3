b716407e89bc705387e5824902d4d94ceb1f10de
Micah Jaffe <micah.jaffe95@gmail.com>
2018-12-05 16:03:39 -0800

    Complete configuration

---
+++ pokedex/frontend/pokedex.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const rootEl = document.getElementById('root');
+  ReactDOM.render(
+    <h1>hello</h1>,
+    rootEl
+  );
+})
