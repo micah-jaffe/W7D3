471dceac00a6d4a2bb8f3955e248bc4d509b05cf
Micah Jaffe <micah.jaffe95@gmail.com>
2018-12-05 17:13:39 -0800

    Complete store configuration

--- pokedex/frontend/pokedex.jsx
+++ pokedex/frontend/pokedex.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('root');
@@ -8,3 +9,13 @@ document.addEventListener('DOMContentLoaded', () => {
     rootEl
   );
 })
+  
+
+// TESTING 
+import { receiveAllPokemon } from './actions/pokemon_actions';
+import fetchAllPokemon from './util/api_util';
+const store = configureStore();
+window.getState = store.getState;
+window.dispatch = store.dispatch;
+window.receiveAllPokemon = receiveAllPokemon;
+window.fetchAllPokemon = fetchAllPokemon;
